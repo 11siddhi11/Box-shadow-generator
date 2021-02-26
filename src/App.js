20b0b2916a6eb3a3ddc637655e0c4e49cf0f2ed0
11siddhi11 <siddhisangamnerkar27@gmail.com>
2021-02-26 23:11:08 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import { useState } from "react"
 function App() {
+  const [horizontal, sethorizaontal] = useState(10)
+  const [vertical, setvertical] = useState(10)
+  const [blur, setblur] = useState(10)
+  const [color, setcolor] = useState("#000")
+  const [checkOn, setcheck] = useState(false)
+
   return (
+    <>
+    <h3>box shadow generator</h3>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="controls">
+
+        <label>Horizontal length</label>
+        <input type="range" min="-200" max="200" value={horizontal} onChange={(e) => { sethorizaontal(e.target.value) }} />
+
+        <label>Vertical length</label>
+        <input type="range" min="-200" max="200" value={vertical} onChange={(e) => { setvertical(e.target.value) }} />
+
+        <label>Blur</label>
+        <input type="range" min="-200" max="200" value={blur} onChange={(e) => { setblur(e.target.value) }} />
+
+        <div className="adjust">
+
+          <label>Color</label>
+          <input type="color" min="-200" max="200" value={color} onChange={(e) => { setcolor(e.target.value) }} />
+        </div>
+        <div className="adjust">
+          <div class="switch">
+            <label>
+              Outline
+      <input type="checkbox" checked={checkOn} onChange={() => { setcheck(!checkOn) }} />
+              <span class="lever"></span>
+              Inset
+    </label>
+          </div>
+
+        </div>
+
+      </div>
+      <div className="output">
+        <div className="box" style={{ boxShadow: `${horizontal}px ${vertical}px ${blur}px ${color}  ${checkOn ? "inset" : ""}` }}>
+          <p>box-shadow:{horizontal}px {vertical}px {blur}px {color} {checkOn ? "inset" : ""}</p>
+
+        </div>
+
+      </div>
     </div>
+    </>
   );
 }
 
